542d270f071b05ad15625dda367584ac934186c4
Senthil Kumaran <senthil@uthcode.com>
2021-06-10 16:14:29 -0700

    Pet Clinic VCS root.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,33 +3,11 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2021.1"
 
 project {
 
-    vcsRoot(HttpsGithubComOrsenthilSpringPetclinicRefsHeadsMain)
+    vcsRoot(PetClinicVcsRoot)
 
     buildType(Build)
 }
@@ -38,7 +16,7 @@ object Build : BuildType({
     name = "Build"
 
     vcs {
-        root(HttpsGithubComOrsenthilSpringPetclinicRefsHeadsMain)
+        root(PetClinicVcsRoot)
     }
 
     steps {
@@ -54,7 +32,7 @@ object Build : BuildType({
     }
 })
 
-object HttpsGithubComOrsenthilSpringPetclinicRefsHeadsMain : GitVcsRoot({
+object PetClinicVcsRoot: GitVcsRoot({
     name = "https://github.com/orsenthil/spring-petclinic#refs/heads/main"
     url = "https://github.com/orsenthil/spring-petclinic"
     branch = "refs/heads/main"
